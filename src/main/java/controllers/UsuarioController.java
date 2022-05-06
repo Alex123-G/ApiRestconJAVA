d28fc0250ef53542ac4f1e3ff12331e1de0edea8
Alex123-G <alexgallozo521@gmial.com>
2022-05-05 18:27:04 -0600

    Subiendo el proyecto a git echo con Spring Boot y Java en Eclipse

---
+++ src/main/java/controllers/UsuarioController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//EN SPRING SE UTILIZAN MUCHO LAS ANOTACION(ESTA SE UTILIZAN COLOCANDO UN ARROBA @) Y EN ESTE CASO
+// LA UTILIZAMOS PARA ESPECIFICAR QUE SE VA UTILIZAR COMO UN CONTROLLADOR PARA UNA API REST
+@RestController
+public class UsuarioController {
+
+	//Añadiendo cambios
+}
