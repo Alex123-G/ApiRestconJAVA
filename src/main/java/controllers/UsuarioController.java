664f93a1551f3b106c862c5329cf988453385c45
Alex Gallozo <alexgallozo521@gmail.com>
2022-05-05 20:08:39 -0500

    Subiendo modificacion al proyecto desde la laptop

--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //EN SPRING SE UTILIZAN MUCHO LAS ANOTACION(ESTA SE UTILIZAN COLOCANDO UN ARROBA @) Y EN ESTE CASO
 // LA UTILIZAMOS PARA ESPECIFICAR QUE SE VA UTILIZAR COMO UN CONTROLLADOR PARA UNA API REST
 @RestController
+@EnableAutoConfiguration
+
 public class UsuarioController {
 
 	//Añadiendo cambios
+	  @RequestMapping("/home")
+	    String home() {
+	        return "Hello World!";
+	    }
 }
