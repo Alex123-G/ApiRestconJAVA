e7bc9bb530075d4c204af78c152bd8e8113ac7b3
Alex123-G <alexgallozo521@gmial.com>
2022-05-09 13:34:49 -0600

    Subiendo cambios js

--- src/main/java/com/CursoLucas/Moy/ApiRest/controller/UsuarioController.java
+++ src/main/java/com/CursoLucas/Moy/ApiRest/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.CursoLucas.Moy.ApiRest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.CursoLucas.Moy.ApiRest.models.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,14 +27,33 @@ public class UsuarioController {
 	}
 
 	@GetMapping(value = "usuario")
-	public Usuario editar() {
-		Usuario usuario = new Usuario();
-		usuario.setNombre("Alex");
-		usuario.setApellido("Gallozo");
-		usuario.setEmail("alexCorreo");
-		usuario.setTelefono("1234");
-		usuario.setPassword(null);
-		return usuario;
+	public List<Usuario> editar() {
+		List<Usuario> usuarios = new ArrayList<>();
+		Usuario usuario1 = new Usuario();
+		usuario1.setNombre("Alex");
+		usuario1.setApellido("Gallozo");
+		usuario1.setEmail("alexCorreo");
+		usuario1.setTelefono("1234");
+		usuario1.setPassword(null);
+
+		Usuario usuario2 = new Usuario();
+		usuario2.setNombre("Lucas");
+		usuario2.setApellido("Marron");
+		usuario2.setEmail("correo");
+		usuario2.setTelefono("asda");
+		usuario2.setPassword("asdasd");
+
+		Usuario usuario3 = new Usuario();
+		usuario3.setNombre("Dilan");
+		usuario3.setApellido("Roco");
+		usuario3.setEmail("afe");
+		usuario3.setTelefono("684asd");
+		usuario3.setPassword("asdads");
+
+		usuarios.add(usuario1);
+		usuarios.add(usuario2);
+		usuarios.add(usuario3);
+		return usuarios;
 	}
 
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,8 +1,15 @@
 // Call the dataTable ususario jQuery plugin
 $(document).ready(function () {
+	consultarUsuarios();
 	$("#usuarios").DataTable();
 });
 
-async function agregarUsuario() {}
-
-const request = await fetch("");
+async function consultarUsuarios() {
+	try {
+		const request = await fetch("http://localhost:8080/usuario");
+		const usuarios = await request.json();
+		console.log(usuarios);
+	} catch (error) {
+		console.log(error);
+	}
+}
